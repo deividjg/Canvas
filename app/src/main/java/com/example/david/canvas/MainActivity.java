8da218efa307f0583c8402f9d30d01fa4b63456f
deividjg <deividjg@gmail.com>
2016-11-29 12:44:14 +0100

    Rev1

--- app/src/main/java/com/example/david/canvas/MainActivity.java
+++ app/src/main/java/com/example/david/canvas/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.david.canvas;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +18,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    RelativeLayout layout1;
+    Pintar pintar;
+    char color = 'b';
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +33,11 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        layout1 = (RelativeLayout) findViewById(R.id.content_main);
+        pintar = new Pintar(this);
+        layout1.addView(pintar);
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -59,11 +75,13 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.restablecer) {
-            return true;
+            layout1.removeAllViews();
+            pintar = new Pintar(this);
+            layout1.addView(pintar);
         }
 
         if (id == R.id.guardar) {
-            return true;
+            color = 'r';
         }
 
         return super.onOptionsItemSelected(item);
@@ -85,10 +103,60 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.cuadrados) {
 
-        } 
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    class Pintar extends View {
+        float x = 50;
+        float y = 50;
+        String accion = "accion";
+        Path path = new Path();
+
+        public Pintar(Context context) {
+            super(context);
+        }
+
+        public void onDraw(Canvas canvas) {
+            Paint paint = new Paint();
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeWidth(5);
+            paint.setColor(color());
+            canvas.drawPath(path, paint);
+
+            if (accion == "down") {
+                canvas.drawPath(path, paint);
+                path.moveTo(x, y);
+            }
+            if (accion == "move") {
+                paint.setColor(color());
+                path.lineTo(x, y);
+            }
+        }
+
+        public int color(){
+            switch (color){
+                case 'b': return Color.BLUE;
+                case 'r': return Color.RED;
+                default: return Color.BLACK;
+            }
+        }
+
+        public boolean onTouchEvent(MotionEvent e) {
+            x = e.getX();
+            y = e.getY();
+
+            if (e.getAction() == MotionEvent.ACTION_DOWN) {
+                accion = "down";
+            }
+            if (e.getAction() == MotionEvent.ACTION_MOVE) {
+                accion = "move";
+            }
+            invalidate();
+            return true;
+        }
     }
 }
