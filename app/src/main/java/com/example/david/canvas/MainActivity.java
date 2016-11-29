61d5dc600614db74fd8749c245062e5d2a9a9749
deividjg <deividjg@gmail.com>
2016-11-29 20:08:15 +0100

    release candidate

--- app/src/main/java/com/example/david/canvas/MainActivity.java
+++ app/src/main/java/com/example/david/canvas/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.david.canvas;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,8 +26,10 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     RelativeLayout layout1;
-    Pintar pintar;
+    Pintar pintarNegro, pintarRojo, pintarVerde, pintarAzul;
     char color = 'n';
+    int grosor = 5;
+    String figura = "libre";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +39,14 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         layout1 = (RelativeLayout) findViewById(R.id.content_main);
-        pintar = new Pintar(this);
-        layout1.addView(pintar);
+        pintarNegro = new Pintar(this);
+        pintarRojo = new Pintar(this);
+        pintarVerde = new Pintar(this);
+        pintarAzul = new Pintar(this);
+        layout1.addView(pintarRojo);
+        layout1.addView(pintarVerde);
+        layout1.addView(pintarAzul);
+        layout1.addView(pintarNegro);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -67,21 +77,62 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.restablecer) {
-            layout1.removeAllViews();
-            pintar = new Pintar(this);
-            layout1.addView(pintar);
-
+        if (id == R.id.rojo) {
+            color = 'r';
+            layout1.removeView(pintarRojo);
+            layout1.addView(pintarRojo);
         }
 
-        if (id == R.id.guardar) {
+        if (id == R.id.verde) {
+            color = 'v';
+            layout1.removeView(pintarVerde);
+            layout1.addView(pintarVerde);
+        }
 
+        if (id == R.id.negro) {
+            color = 'n';
+            layout1.removeView(pintarNegro);
+            layout1.addView(pintarNegro);
+        }
+
+        if (id == R.id.azul) {
+            color = 'a';
+            layout1.removeView(pintarAzul);
+            layout1.addView(pintarAzul);
+        }
+
+        if (id == R.id.fino) {
+            grosor = 5;
+        }
+
+        if (id == R.id.medio) {
+            grosor = 20;
+        }
+
+        if (id == R.id.grueso) {
+            grosor = 50;
+        }
+
+        if (id == R.id.fRojo) {
+            layout1.setBackgroundColor(Color.RED);
+        }
+
+        if (id == R.id.fVerde) {
+            layout1.setBackgroundColor(Color.GREEN);
+        }
+
+        if (id == R.id.fAzul) {
+            layout1.setBackgroundColor(Color.BLUE);
+        }
+
+        if (id == R.id.fNegro) {
+            layout1.setBackgroundColor(Color.BLACK);
+        }
+
+        if (id == R.id.fBlanco) {
+            layout1.setBackgroundColor(Color.WHITE);
         }
 
         return super.onOptionsItemSelected(item);
@@ -90,23 +141,26 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.libre) {
-            // Handle the camera action
-        } else if (id == R.id.cuadrado) {
-
+        if (id == R.id.restablecer) {
+            layout1.removeAllViews();
+            pintarNegro = new Pintar(this);
+            pintarRojo = new Pintar(this);
+            pintarVerde = new Pintar(this);
+            pintarAzul = new Pintar(this);
+            layout1.addView(pintarRojo);
+            layout1.addView(pintarVerde);
+            layout1.addView(pintarAzul);
+            layout1.addView(pintarNegro);
+        } else if (id == R.id.salir) {
+            System.exit(0);
         } else if (id == R.id.circulo) {
-
-        } else if (id == R.id.rojo) {
-            color = 'r';
-        } else if (id == R.id.verde) {
-            color = 'v';
-        } else if (id == R.id.azul) {
-            color = 'a';
-        } else if (id == R.id.negro) {
-            color = 'n';
+            figura = "circulo";
+        } else if (id == R.id.cuadrado) {
+            figura = "cuadrado";
+        } else if (id == R.id.libre) {
+            figura = "libre";
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -127,17 +181,27 @@ public class MainActivity extends AppCompatActivity
         public void onDraw(Canvas canvas) {
             Paint paint = new Paint();
             paint.setStyle(Paint.Style.STROKE);
-            paint.setStrokeWidth(5);
+            paint.setStrokeWidth(grosor);
             paint.setColor(color());
             canvas.drawPath(path, paint);
 
-            if (accion == "down") {
+            if(figura.equals("libre")){
                 canvas.drawPath(path, paint);
-                path.moveTo(x, y);
+
+                if (accion == "down") {
+                    path.moveTo(x, y);
+                }
+                if (accion == "move") {
+                    path.lineTo(x, y);
+                }
             }
-            if (accion == "move") {
-                paint.setColor(color());
-                path.lineTo(x, y);
+
+            if(figura.equals("cuadrado")) {
+                canvas.drawRect(x, y, x + 250, y + 150, paint);
+            }
+
+            if(figura.equals("circulo")) {
+                canvas.drawCircle(x, y, 100, paint);
             }
         }
 
@@ -146,10 +210,10 @@ public class MainActivity extends AppCompatActivity
                 case 'a': return Color.BLUE;
                 case 'r': return Color.RED;
                 case 'v': return Color.GREEN;
-                default: return Color.BLACK;
+                case 'n': return Color.BLACK;
             }
+            return Color.BLACK;
         }
-
 
         public boolean onTouchEvent(MotionEvent e) {
             x = e.getX();
@@ -161,6 +225,7 @@ public class MainActivity extends AppCompatActivity
             if (e.getAction() == MotionEvent.ACTION_MOVE) {
                 accion = "move";
             }
+
             invalidate();
             return true;
         }
